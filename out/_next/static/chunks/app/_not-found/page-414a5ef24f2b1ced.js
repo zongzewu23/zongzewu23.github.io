61bdf349456676c7cedb8cc9df963578ab73148b
Zongze Wu <astrowu61@gmail.com>
2025-01-02 22:30:10 -0800

    update

---
+++ out/_next/static/chunks/app/_not-found/page-414a5ef24f2b1ced.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[409],{7303:function(n,e,_){Promise.resolve().then(_.t.bind(_,7210,23))},7210:function(n){n.exports={style:{fontFamily:"'__Syne_b40857', '__Syne_Fallback_b40857'",fontStyle:"normal"},className:"__className_b40857"}}},function(n){n.O(0,[779,971,117,744],function(){return n(n.s=7303)}),_N_E=n.O()}]);
